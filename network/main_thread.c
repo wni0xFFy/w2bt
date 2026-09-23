7a8e80fc8e09a0ccb84320443c2b4e37ef5bcc69
wni0xFFy <makar10l.mail@proton.me>
2026-09-23 17:15:19 +0300

    upd : logging information(i just added beautiful output lol

--- network/main_thread.c
+++ network/main_thread.c
@@ -11,15 +11,29 @@ int main(){
 	setbuf(stdout, NULL);
 
 	sock_t* fd = create_socket(25656, 5);
-	if(fd == NULL) return -1;
+	if(fd == NULL){
+		fprintf(stderr, "Couldn't open socket!");
+		return -1;
+	}
+
+	fprintf(stdout, "[INIT] SOCKET OPENED\n");
+
 	nonblocking_socket(fd);
 
 	fd_poll_t* epfd = create_sockets_poll(10, -1);
-	if(epfd == NULL) return -1;
+	if(epfd == NULL){
+		fprintf(stderr, "Couldn't open create epoll!");
+	 	return -1;
+	}
+
+	fprintf(stdout, "[INIT] EPOLL CREATED\n");
 
 	add_socket_event(epfd, fd, ACCEPT, EPOLLIN);
 
-	fprintf(stdout, "waiting..\n");
+	fprintf(stdout, "[INIT] SOCKET ADDED TO EPOLL\n");
+
+	fprintf(stdout, "[RUNNING] INITIATION SUCCESSFUL\n");
+
 	uint8_t* buffer = malloc(32512);
 	while(1){
 		int s = 0;
@@ -28,33 +42,30 @@ int main(){
 		for(int i = 0; i < s; i++){
 			if(tasks[i]->state == ACCEPT){	
 				accept_handler(fd, epfd);
-				fprintf(stdout, "%d : CLIENT %x CONNECTES\n", i, tasks[i]);
+				fprintf(stdout, "[RUNNING] CLIENT CONNECTED\n");
 			}
 			else if(tasks[i]->state == IN){
 				read_handler(tasks[i], buffer, 256);
 				buffer[255] = '\0';
-				fprintf(stdout, "%d : CLIENT SEND\n", i);
+				fprintf(stdout, "[RUNNING] CLIENT SENDED SOMETHING\n");
 				for(int i = 0; i < 256; i++){
 					buffer[i] = 0;
 				}
 			}
 			else if(tasks[i]->state == CLOSED){
-				if(tasks[i]->state == CLOSED) fprintf(stdout, "CONNECTION %x BY CLIENT\n", tasks[i]);
+				if(tasks[i]->state == CLOSED) fprintf(stdout, "[RUNNING] CONNECTION CLOSED BY CLIENT\n");
  				close_handler(epfd, tasks[i]);
 			}
 
 			else{
-				fprintf(stdout, "%d : GHOST SOCKET %x\n ", i, tasks[i]);
+				fprintf(stderr, "ghost socket. wrong state. data pointer 0x%x\n", (unsigned int)tasks[i]);
 				free(tasks[i]);
 				break;
 			}
 		}
 		free(tasks);
-		fprintf(stdout, "events : %d\n", s);
-		fprintf(stdout, "c1 : %d\n", epfd->tasks_lenght);
 	}
 	free(buffer);
-	fprintf(stdout, "c : %d", epfd->tasks_lenght);
 	delete_sockets_poll(epfd);
 	return -1;
 }
