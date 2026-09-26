e6152d6b538daee34bc7f529d5ab55b27142c2bd
wni0xFFy <makar10l.mail@proton.me>
2026-09-26 12:25:47 +0300

    added : separated network module to other thread

--- core/main.c
+++ core/main.c
@@ -2,7 +2,24 @@
 #include <w2bt/core/socket.h>	
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+
+typedef struct {
+	fd_poll_t* epl;
+	sock_t* srv;
+} network_args;
+
+void* network_worker(void* args){
+	network_args* arg = args;
+	fd_poll_t* epl = arg->epl;
+	sock_t* srv = arg->srv;
+
+	network_main(epl, srv);
+}
+
 int main(){
+	pthread_t network_main_thread;
+
 	fd_poll_t* epl = create_poll(15, 30000);
 	if(epl == NULL){
 		printf("[ERROR] EPOLL NOT STARTED");
@@ -14,6 +31,13 @@ int main(){
 		return -2;
 	}
 
-	network_main(epl, server);
+	network_args args = {0};
+	args.epl = epl;
+	args.srv = server;
+
+	pthread_create(&network_main_thread, NULL, network_worker, &args);
+	printf("[Network] Started.\n");
+	printf("[Network] working.\n");
+	pthread_join(network_main_thread, NULL);	
 	delete_poll(epl);	
 }
